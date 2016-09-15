e6bcbfc36d4c5ca4ff3d62ad2190f47b42ace57f
kilo aka Gabor Kmetyko <kg.kilo@gmail.com>
2016-09-15 22:05:25 +0200

    Read connection string settings from Registry

--- szet_64/FormStart.vb
+++ szet_64/FormStart.vb
@@ -1,6 +1,10 @@
-﻿Public Class FormStart
+﻿Imports System.Data.SqlClient
+
+Public Class FormStart
+    Private sConnStr As String
 
     Private Sub cmdMunkalap_Click(sender As Object, e As EventArgs) Handles cmdMunkalap.Click
+        Form001.sConnStr = Me.sConnStr
         Form001.Show(Me)
     End Sub
 
@@ -14,5 +18,20 @@
 
     Private Sub cmdExit_Click(sender As Object, e As EventArgs) Handles cmdExit.Click
         Me.Close()
+    End Sub
+
+    Private Sub FormStart_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Read connection string from the registry
+        Dim keyValue As Object
+        keyValue = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\Formel\SZET_64\Settings", "ConnectionName", "Server=(localdb)\v11.0;Integrated Security=true;AttachDbFileName=C:\Users\kilo\Documents\SZETAV.mdf;")
+        If (keyValue) Then
+            sConnStr = keyValue.ToString
+        Else
+            MsgBox("bing")
+            sConnStr = "Server=(localdb)\v11.0;Integrated Security=true;AttachDbFileName=C:\Users\kilo\Documents\SZETAV.mdf;"
+        End If
+        'My.Settings.Item("ConnectionString") = keyValue
+        'SZETAVDataSet...
+
     End Sub
 End Class
