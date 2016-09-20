8f58e0bea276d1b3e1b42c123a83e07fb7b747cd
kilo aka Gabor Kmetyko <kg.kilo@gmail.com>
2016-09-20 22:32:34 +0200

    Read ConnectionString from Registry and set it in-app.

--- szet_64/FormStart.vb
+++ szet_64/FormStart.vb
@@ -23,16 +23,24 @@ Public Class FormStart
 
     Private Sub FormStart_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Read connection string from the registry
-        Dim keyValue As Object
-        keyValue = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\Formel\SZET_64\Settings", "ConnectionName", "Server=(localdb)\v11.0;Integrated Security=true;AttachDbFileName=C:\Users\kilo\Documents\SZETAV.mdf;")
-        If (keyValue) Then
-            sConnStr = keyValue.ToString
-        Else
-            MsgBox("bing")
-            sConnStr = "Server=(localdb)\v11.0;Integrated Security=true;AttachDbFileName=C:\Users\kilo\Documents\SZETAV.mdf;"
-        End If
-        'My.Settings.Item("ConnectionString") = keyValue
-        'SZETAVDataSet...
+        sConnStr = "Server=(localdb)\v11.0;Integrated Security=true;AttachDbFileName=C:\Users\kilo\Documents\SZETAV.mdf;"
+        Try
+            Dim keyValue As Object
+            'Ez valamiert nem mukodik, pedig a connection az gep szintu beallitas kellene legyen, nem user-szintu.
+            'Esetleg valami jogosultsag???
+            'keyValue = My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Formel\SZET_64", "ConnectionString", "Server=(localdb)\v11.0;Integrated Security=true;AttachDbFileName=C:\Users\kilo\Documents\SZETAV.mdf;")
+
+            'Ez mukodik, de user-specifikus :(
+            keyValue = My.Computer.Registry.GetValue("HKEY_CURRENT_USER\SOFTWARE\Formel\SZET_64", _
+                                                     "ConnectionString", _
+                                                     "Server=(localdb)\v11.0;Integrated Security=true;AttachDbFileName=C:\Users\kilo\Documents\SZETAV.mdf;")
+
+            sConnStr = If(keyValue.ToString, "")
+        Catch ex As Exception
+            MsgBox(ex.Message, , ex.ToString)
+        End Try
+
+        My.Settings.Item("SZETAVConnectionString") = sConnStr   'Ez allitja be az ossze bound DataSet eleresi utjat!!!
 
     End Sub
 End Class
