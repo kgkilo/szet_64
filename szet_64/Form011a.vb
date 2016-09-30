a4ad2e32b0489ec233d06f5045bcb5dd7ec47894
kilo aka Gabor Kmetyko <kg.kilo@gmail.com>
2016-09-30 17:50:22 +0200

    Form011a - Hide unused controls.
    Hides all the controls that are not use by an object type,
    reading the settings from the PERMIS table.

--- szet_64/Form011a.vb
+++ szet_64/Form011a.vb
@@ -46,6 +46,7 @@ Public Class Form011a
                         cmbNEVLTELJME.SelectedValue = sqlReader.Item("NEVLTELJME")
                         cmbFSZAM.SelectedValue = sqlReader.Item("FSZAM")
                         txtMEGJ.Text = sqlReader.Item("MEGJ").ToString
+                        txtOBJTIP.Text = sqlReader.Item("OBJTIP").ToString
                     End While
                 End If   'HasRows
                 sqlReader.Close()
@@ -65,9 +66,9 @@ Public Class Form011a
             cmbUZALL.SelectedIndex = -1
             cmbVAROS.SelectedIndex = -1
             cmbVESZO.SelectedIndex = -1
-
         End If
 
+        Me.HideUnusedControls()
     End Sub
 
     Private Sub cmdCANCEL_Click(sender As Object, e As EventArgs) Handles cmdCANCEL.Click
@@ -92,13 +93,7 @@ Public Class Form011a
                 .Parameters.AddWithValue("GYSZAM", txtGYSZAM.Text)
                 .Parameters.AddWithValue("GYARTO", cmbGYARTO.SelectedValue)
                 .Parameters.AddWithValue("UZALL", cmbUZALL.SelectedValue)
-                'If dtUZEMIDO.Value.EndsWith(".") Then
-                '    txtUZEMIDO.Text = txtUZEMIDO.Text.Remove(txtUZEMIDO.TextLength - 1)
-                'End If
                 .Parameters.AddWithValue("UZEMIDO", dtUZEMIDO.Value)
-                'If txtHITIDO.Text.EndsWith(".") Then
-                '    txtHITIDO.Text = txtHITIDO.Text.Remove(txtHITIDO.TextLength - 1)
-                'End If
                 .Parameters.AddWithValue("HITIDO", dtHITIDO.Value)
                 .Parameters.AddWithValue("GYARTM", txtGYARTM.Text)
                 .Parameters.AddWithValue("TELEPHSZ", txtTELEPHSZ.Text)
@@ -136,4 +131,57 @@ Public Class Form011a
         lblTitle.Left = (Me.Width - lblTitle.Size.Width) / 2
     End Sub
 
+    Private Sub HideUnusedControls()
+        'Ez a tomb tartalmazza azoknak a control elemeknek a nevet,
+        'amiknek meg kell jelenni a dialog-on
+        Dim EnableThese(130) As String
+
+        'Beolvassuk a PERMIS tablabol, hogy az adott objektumtipusnak melyik mezok kellenek
+        sqlConn = New SqlConnection(GlobalVars.sConnStr)
+        Dim sqlComm As New SqlCommand("sp_GetPermis", sqlConn)
+        sqlComm.CommandType = CommandType.StoredProcedure
+        sqlComm.Parameters.Add("@pOBJTIP", SqlDbType.VarChar, 2).Value = txtOBJTIP.Text
+        sqlConn.Open()
+        Dim sqlReader As SqlDataReader = sqlComm.ExecuteReader()
+        If sqlReader.HasRows Then
+            While (sqlReader.Read())    'A PERMIS tablaban a mezo neve=a control neve.
+                Try                     'Ha 1 a mezo erteke, akkor meg kell jeleniteni,
+                    Dim j As Integer = 0    'azaz akkor betesszuk a string tombbe
+                    For i As Integer = 0 To 129 Step 1  '129 oszlop van a PERMIS tablaban...
+                        If sqlReader.Item(i) > 0 Then
+                            EnableThese(j) = sqlReader.GetName(i).ToString()    'tomb<-mezo neve
+                            j = j + 1
+                        End If
+                    Next    'For i
+                Catch ex As Exception
+                    MsgBox("túlsok")
+                End Try
+            End While   'sqlReader.Read
+        End If   'HasRows
+        sqlReader.Close()
+        sqlReader.Dispose()
+        sqlComm.Dispose()
+        sqlConn.Dispose()
+
+        'Most pedig vegigmegyunk a dialogbox osszes control-jan es megnezzuk,
+        'hogy a neve szerepel-e a megjelenitendok listajan. Ha igen, akkor kirakjuk,
+        'ha nem, akkor elrejtjuk es letiltjuk.
+        'Ezt csak azokra a control-okra kell lejatszani, amiknek a neve txt, cmb, dt, lbl kezdetu.
+        Dim o As Control
+        For Each o In Me.TabControl1.TabPages(0).Controls
+            Dim sName As String
+            sName = If(o.Name.StartsWith("dt"), o.Name.Substring(2), o.Name.Substring(3))
+            If o.Name.StartsWith("txt") Or o.Name.StartsWith("cmb") Or o.Name.StartsWith("lbl") _
+                Or o.Name.StartsWith("dt") Then
+                If EnableThese.Contains(sName) Then
+                    o.Visible = True
+                    o.Enabled = True
+                Else
+                    o.Visible = False
+                    o.Enabled = False
+                End If
+            End If
+        Next
+
+    End Sub
 End Class
