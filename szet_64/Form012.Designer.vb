8b8508655c3af5e8d098179dde20abc80e74714b
kilo aka Gabor Kmetyko <kg.kilo@gmail.com>
2016-10-12 01:17:50 +0200

    Form012 - Bottom grids work as per design.

--- szet_64/Form012.Designer.vb
+++ szet_64/Form012.Designer.vb
@@ -25,22 +25,33 @@ Partial Class Form012
         Me.components = New System.ComponentModel.Container()
         Me.lblTitle = New System.Windows.Forms.Label()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblTALALAT = New System.Windows.Forms.Label()
+        Me.txtTALALAT = New System.Windows.Forms.TextBox()
         Me.grdMEGNEV = New System.Windows.Forms.DataGridView()
         Me.lblOBJTIP = New System.Windows.Forms.Label()
         Me.cmbOBJTIP = New System.Windows.Forms.ComboBox()
         Me.SpQ296BindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.SZETAVDataSet = New szet_64.SZETAVDataSet()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.cmdODA = New System.Windows.Forms.Button()
+        Me.cmdVISSZA = New System.Windows.Forms.Button()
+        Me.lblKAPCSOLT = New System.Windows.Forms.Label()
+        Me.lblNEMKAPCSOLT = New System.Windows.Forms.Label()
+        Me.grdKAPCSOLT = New System.Windows.Forms.DataGridView()
+        Me.grdNEMKAPCSOLT = New System.Windows.Forms.DataGridView()
         Me.cmbOBJTIP2 = New System.Windows.Forms.ComboBox()
+        Me.SpQ296BindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
         Me.lblOBJTIP2 = New System.Windows.Forms.Label()
         Me.Sp_Q296TableAdapter = New szet_64.SZETAVDataSetTableAdapters.sp_Q296TableAdapter()
-        Me.lblTALALAT = New System.Windows.Forms.Label()
-        Me.txtTALALAT = New System.Windows.Forms.TextBox()
+        Me.cmdCLOSE = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
         CType(Me.grdMEGNEV, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.SpQ296BindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.SZETAVDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupBox2.SuspendLayout()
+        CType(Me.grdKAPCSOLT, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.grdNEMKAPCSOLT, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SpQ296BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'lblTitle
@@ -66,6 +77,27 @@ Partial Class Form012
         Me.GroupBox1.TabIndex = 12
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Berendezés választás"
+        '
+        'lblTALALAT
+        '
+        Me.lblTALALAT.Location = New System.Drawing.Point(660, 22)
+        Me.lblTALALAT.Name = "lblTALALAT"
+        Me.lblTALALAT.Size = New System.Drawing.Size(75, 13)
+        Me.lblTALALAT.TabIndex = 17
+        Me.lblTALALAT.Text = "Találatok"
+        Me.lblTALALAT.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtTALALAT
+        '
+        Me.txtTALALAT.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtTALALAT.Enabled = False
+        Me.txtTALALAT.Font = New System.Drawing.Font("Arial", 8.25!)
+        Me.txtTALALAT.Location = New System.Drawing.Point(741, 19)
+        Me.txtTALALAT.Name = "txtTALALAT"
+        Me.txtTALALAT.ReadOnly = True
+        Me.txtTALALAT.Size = New System.Drawing.Size(75, 20)
+        Me.txtTALALAT.TabIndex = 16
+        Me.txtTALALAT.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'grdMEGNEV
         '
@@ -111,17 +143,82 @@ Partial Class Form012
         '
         'GroupBox2
         '
+        Me.GroupBox2.Controls.Add(Me.cmdODA)
+        Me.GroupBox2.Controls.Add(Me.cmdVISSZA)
+        Me.GroupBox2.Controls.Add(Me.lblKAPCSOLT)
+        Me.GroupBox2.Controls.Add(Me.lblNEMKAPCSOLT)
+        Me.GroupBox2.Controls.Add(Me.grdKAPCSOLT)
+        Me.GroupBox2.Controls.Add(Me.grdNEMKAPCSOLT)
         Me.GroupBox2.Controls.Add(Me.cmbOBJTIP2)
         Me.GroupBox2.Controls.Add(Me.lblOBJTIP2)
         Me.GroupBox2.Location = New System.Drawing.Point(13, 230)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(822, 246)
+        Me.GroupBox2.Size = New System.Drawing.Size(822, 315)
         Me.GroupBox2.TabIndex = 13
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Kapcsolatok"
         '
+        'cmdODA
+        '
+        Me.cmdODA.Location = New System.Drawing.Point(375, 141)
+        Me.cmdODA.Name = "cmdODA"
+        Me.cmdODA.Size = New System.Drawing.Size(75, 23)
+        Me.cmdODA.TabIndex = 25
+        Me.cmdODA.Text = "=====>"
+        Me.cmdODA.UseVisualStyleBackColor = True
+        '
+        'cmdVISSZA
+        '
+        Me.cmdVISSZA.Location = New System.Drawing.Point(375, 213)
+        Me.cmdVISSZA.Name = "cmdVISSZA"
+        Me.cmdVISSZA.Size = New System.Drawing.Size(75, 23)
+        Me.cmdVISSZA.TabIndex = 25
+        Me.cmdVISSZA.Text = "<====="
+        Me.cmdVISSZA.UseVisualStyleBackColor = True
+        '
+        'lblKAPCSOLT
+        '
+        Me.lblKAPCSOLT.Location = New System.Drawing.Point(456, 53)
+        Me.lblKAPCSOLT.Name = "lblKAPCSOLT"
+        Me.lblKAPCSOLT.Size = New System.Drawing.Size(360, 13)
+        Me.lblKAPCSOLT.TabIndex = 16
+        Me.lblKAPCSOLT.Text = "K A P C S O L V A"
+        Me.lblKAPCSOLT.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblNEMKAPCSOLT
+        '
+        Me.lblNEMKAPCSOLT.Location = New System.Drawing.Point(9, 53)
+        Me.lblNEMKAPCSOLT.Name = "lblNEMKAPCSOLT"
+        Me.lblNEMKAPCSOLT.Size = New System.Drawing.Size(360, 13)
+        Me.lblNEMKAPCSOLT.TabIndex = 16
+        Me.lblNEMKAPCSOLT.Text = "N I N C S   K A P C S O L V A"
+        Me.lblNEMKAPCSOLT.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'grdKAPCSOLT
+        '
+        Me.grdKAPCSOLT.AllowUserToAddRows = False
+        Me.grdKAPCSOLT.AllowUserToDeleteRows = False
+        Me.grdKAPCSOLT.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdKAPCSOLT.Location = New System.Drawing.Point(456, 69)
+        Me.grdKAPCSOLT.Name = "grdKAPCSOLT"
+        Me.grdKAPCSOLT.ReadOnly = True
+        Me.grdKAPCSOLT.Size = New System.Drawing.Size(360, 240)
+        Me.grdKAPCSOLT.TabIndex = 15
+        '
+        'grdNEMKAPCSOLT
+        '
+        Me.grdNEMKAPCSOLT.AllowUserToAddRows = False
+        Me.grdNEMKAPCSOLT.AllowUserToDeleteRows = False
+        Me.grdNEMKAPCSOLT.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdNEMKAPCSOLT.Location = New System.Drawing.Point(9, 69)
+        Me.grdNEMKAPCSOLT.Name = "grdNEMKAPCSOLT"
+        Me.grdNEMKAPCSOLT.ReadOnly = True
+        Me.grdNEMKAPCSOLT.Size = New System.Drawing.Size(360, 240)
+        Me.grdNEMKAPCSOLT.TabIndex = 15
+        '
         'cmbOBJTIP2
         '
+        Me.cmbOBJTIP2.DataSource = Me.SpQ296BindingSource1
         Me.cmbOBJTIP2.DisplayMember = "KODENEV"
         Me.cmbOBJTIP2.FormattingEnabled = True
         Me.cmbOBJTIP2.Location = New System.Drawing.Point(148, 19)
@@ -130,6 +227,11 @@ Partial Class Form012
         Me.cmbOBJTIP2.TabIndex = 13
         Me.cmbOBJTIP2.Tag = "296"
         Me.cmbOBJTIP2.ValueMember = "KODERT"
+        '
+        'SpQ296BindingSource1
+        '
+        Me.SpQ296BindingSource1.DataMember = "sp_Q296"
+        Me.SpQ296BindingSource1.DataSource = Me.SZETAVDataSet
         '
         'lblOBJTIP2
         '
@@ -144,32 +246,21 @@ Partial Class Form012
         '
         Me.Sp_Q296TableAdapter.ClearBeforeFill = True
         '
-        'lblTALALAT
+        'cmdCLOSE
         '
-        Me.lblTALALAT.Location = New System.Drawing.Point(660, 22)
-        Me.lblTALALAT.Name = "lblTALALAT"
-        Me.lblTALALAT.Size = New System.Drawing.Size(75, 13)
-        Me.lblTALALAT.TabIndex = 17
-        Me.lblTALALAT.Text = "Találatok"
-        Me.lblTALALAT.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'txtTALALAT
-        '
-        Me.txtTALALAT.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.txtTALALAT.Enabled = False
-        Me.txtTALALAT.Font = New System.Drawing.Font("Arial", 8.25!)
-        Me.txtTALALAT.Location = New System.Drawing.Point(741, 19)
-        Me.txtTALALAT.Name = "txtTALALAT"
-        Me.txtTALALAT.ReadOnly = True
-        Me.txtTALALAT.Size = New System.Drawing.Size(75, 20)
-        Me.txtTALALAT.TabIndex = 16
-        Me.txtTALALAT.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.cmdCLOSE.Location = New System.Drawing.Point(760, 551)
+        Me.cmdCLOSE.Name = "cmdCLOSE"
+        Me.cmdCLOSE.Size = New System.Drawing.Size(75, 23)
+        Me.cmdCLOSE.TabIndex = 25
+        Me.cmdCLOSE.Text = "Kilépés"
+        Me.cmdCLOSE.UseVisualStyleBackColor = True
         '
         'Form012
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(847, 586)
+        Me.Controls.Add(Me.cmdCLOSE)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.lblTitle)
@@ -182,6 +273,9 @@ Partial Class Form012
         CType(Me.SpQ296BindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.SZETAVDataSet, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupBox2.ResumeLayout(False)
+        CType(Me.grdKAPCSOLT, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.grdNEMKAPCSOLT, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SpQ296BindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -199,4 +293,12 @@ Partial Class Form012
     Friend WithEvents grdMEGNEV As System.Windows.Forms.DataGridView
     Friend WithEvents lblTALALAT As System.Windows.Forms.Label
     Friend WithEvents txtTALALAT As System.Windows.Forms.TextBox
+    Friend WithEvents SpQ296BindingSource1 As System.Windows.Forms.BindingSource
+    Friend WithEvents lblNEMKAPCSOLT As System.Windows.Forms.Label
+    Friend WithEvents grdNEMKAPCSOLT As System.Windows.Forms.DataGridView
+    Friend WithEvents cmdCLOSE As System.Windows.Forms.Button
+    Friend WithEvents cmdODA As System.Windows.Forms.Button
+    Friend WithEvents cmdVISSZA As System.Windows.Forms.Button
+    Friend WithEvents lblKAPCSOLT As System.Windows.Forms.Label
+    Friend WithEvents grdKAPCSOLT As System.Windows.Forms.DataGridView
 End Class
--- szet_64/Form012.vb
+++ szet_64/Form012.vb
@@ -5,12 +5,20 @@ Public Class Form012
     Private Sub Form012_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.Sp_Q296TableAdapter.Fill(Me.SZETAVDataSet.sp_Q296)
         Me.cmbOBJTIP.SelectedIndex = -1
+        Me.cmbOBJTIP2.SelectedIndex = -1
+        Me.cmbOBJTIP2.Enabled = False
+        Me.grdNEMKAPCSOLT.Enabled = False
+        Me.grdKAPCSOLT.Enabled = False
+        Me.cmdODA.Enabled = False
+        Me.cmdVISSZA.Enabled = False
     End Sub
 
     Private Sub cmbOBJTIP_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbOBJTIP.SelectedIndexChanged
         Cursor.Current = Cursors.WaitCursor
 
         If cmbOBJTIP.SelectedIndex <> -1 Then
+            'cmbOBJTIP2.Enabled = False
+
             'Grid feltoltese tarolt eljarasbol
             Dim dbadp As New SqlDataAdapter("sp_FillObjMegnev2", GlobalVars.sConnStr)
             With dbadp.SelectCommand
@@ -59,5 +67,52 @@ Public Class Form012
 
         End If
         Cursor.Current = Cursors.Default
+    End Sub
+
+    Private Sub cmdCLOSE_Click(sender As Object, e As EventArgs) Handles cmdCLOSE.Click, cmdVISSZA.Click, cmdODA.Click
+        Me.Close()
+    End Sub
+
+    Private Sub cmbOBJTIP2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbOBJTIP2.SelectedIndexChanged
+        If cmbOBJTIP2.SelectedIndex <> -1 Then
+            grdNEMKAPCSOLT.Enabled = True
+            grdKAPCSOLT.Enabled = True
+            cmdODA.Enabled = True
+            cmdVISSZA.Enabled = True
+
+            'Nem kapcsolt grid feltoltese
+            Using dbadp As New SqlDataAdapter("sp_FillNotConnected", GlobalVars.sConnStr)
+                With dbadp.SelectCommand
+                    .CommandType = CommandType.StoredProcedure
+                    .Parameters.Add("@pOBJTIP", SqlDbType.VarChar, 2).Value = cmbOBJTIP2.SelectedValue
+                    .Parameters.Add("@pID", SqlDbType.Int).Value = grdMEGNEV.SelectedRows(0).Cells(0).Value
+                    Dim dtNemKapcsolt As New DataTable
+                    dbadp.Fill(dtNemKapcsolt)
+                    dbadp.Dispose()
+                    grdNEMKAPCSOLT.DataSource = dtNemKapcsolt
+                End With
+            End Using
+
+            'Kapcsolt grid feltoltese
+            Using dbadp As New SqlDataAdapter("sp_FillConnected", GlobalVars.sConnStr)
+                With dbadp.SelectCommand
+                    .CommandType = CommandType.StoredProcedure
+                    .Parameters.Add("@pOBJTIP", SqlDbType.VarChar, 2).Value = cmbOBJTIP2.SelectedValue
+                    .Parameters.Add("@pID", SqlDbType.Int).Value = grdMEGNEV.SelectedRows(0).Cells(0).Value
+                    Dim dtKapcsolt As New DataTable
+                    dbadp.Fill(dtKapcsolt)
+                    dbadp.Dispose()
+                    grdKAPCSOLT.DataSource = dtKapcsolt
+                End With
+            End Using
+        End If
+    End Sub
+
+    Private Sub grdMEGNEV_SelectionChanged(sender As Object, e As EventArgs) Handles grdMEGNEV.SelectionChanged
+        cmbOBJTIP2.Enabled = True
+    End Sub
+
+    Private Sub grdMEGNEV_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles grdMEGNEV.CellContentClick
+        cmbOBJTIP2.Enabled = True
     End Sub
 End Class
